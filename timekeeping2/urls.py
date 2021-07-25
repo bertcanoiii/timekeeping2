d70b2f0e8f8fc230e0d47bea3001abe8a1fb75bc
Bert Cano <bertcanoiii@gmail.com>
2021-07-25 15:54:00 -0700

    create login and registration templates

--- timekeeping2/urls.py
+++ timekeeping2/urls.py
@@ -14,8 +14,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+
+from django_registration.backends.one_step.views import RegistrationView
+
+from core.views import IndexTemplateView
+from users.forms import CustomUserForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Since we are using a customer user class/model, we need to use a custom-registration-form
+    path('accounts/register/',
+         RegistrationView.as_view(
+             form_class=CustomUserForm,
+             success_url="/"),
+         name="django_registration_register"),
+
+    # Login through the browser
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Other urls used by the django_registration package
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+
+    # Endpoint for current user login
+    path('api/', include('users.api.urls')),
+
+    # allows login via browsable API
+    path('api-auth/', include('rest_framework.urls')),
+
+    # allows login via REST
+    path('api/rest-auth/', include('rest_auth.urls')),
+
+    # allows registration via REST
+    path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+
+    re_path(r'^.*$', IndexTemplateView.as_view(), name='entry-point'),
+
 ]
